157da38cddae164ffeec1f0e2b40dde61073c349
pawleq <57144337+pawleq@users.noreply.github.com>
2020-10-22 11:45:02 +0300

    v0.1-3
    added 25000x10x100x500x1000 strings testing

--- functions.cpp
+++ functions.cpp
@@ -4,6 +4,10 @@ int coll = 0;
 double minDif = 1.0;
 double totalDif = 0;
 double maxDif = 0;
+double totalDifHex = 0;
+double minDifHex = 1.0;
+double maxDifHex = 0;
+int lengths[4]= {10,100,500,100};
 
 string hashing(string text = "") //mixuojame stringo ascii values su ivairiom operacijomis, apkeitinejam bitus
 {
@@ -31,7 +35,7 @@ string hashing(string text = "") //mixuojame stringo ascii values su ivairiom op
         }
         if (hashOut.length() > 64)
         {
-            hashOut = hashOut.substr(0, 64); //substractina iki 16 ilgio hasho
+            hashOut = hashOut.substr(0, 64); //substractina iki 64 ilgio hasho
         }
     }
     return hashOut;
@@ -51,20 +55,22 @@ string alphaNumericStrings(int length)   //stack overflow funkcija random alpha-
 void test4RandomWords()   //
 {
     cout << "Testing....." << "\n";
-    for (int i = 0; i < RCOUNT; i++)
+    for (int i = 0; i < 4; i++)
     {
-        string wordA = alphaNumericStrings(RLENGTH);
-        string wordB = alphaNumericStrings(RLENGTH);
-        if (hashing(wordA) == hashing(wordB))
+        for (int j = 0; j < 25000; j++)
         {
-            coll++;
+            string wordA = alphaNumericStrings(lengths[i]);
+            string wordB = alphaNumericStrings(lengths[i]);
+            if (hashing(wordA) == hashing(wordB))
+            {
+                coll++;
+            }
         }
+        cout << "Rasta collision'u su "<<lengths[i]<<" simboliu stringais: " << coll << "\n";
+        if (coll == 0) //labai reikalinga salyga
+            cout<<"Hash funkcija ganetinai saugi."<<endl;
     }
-    cout << "Rasta collision'u : " << coll << "\n";
-    if (coll == 0) //labai reikalinga salyga
-        cout<<"Hash funkcija ganetinai saugi."<<endl;
 }
-
 string binary(string word)   //funkcija stringo kiekviena simboli keiciantis i binary
 {
     string binary = "";
@@ -75,7 +81,7 @@ string binary(string word)   //funkcija stringo kiekviena simboli keiciantis i b
     return binary; //returnina binary stringo reiksme
 }
 
-double compareInput (string inputA, string inputB)   //comparina identical inputo poras
+double compareInput (string inputA, string inputB)   //comparina inputo poras
 {
     int identical = 0;
     double perc;
@@ -95,43 +101,67 @@ void testSimilar()
 {
     cout << "Similar words collision test" << "\n";
 
-    for (int i = 0; i < RCOUNT; i++)   //i cikla deti skaiciu, kuris parodo kiek pory tikrinsim
+    for (int i = 0; i < 4; i++)   //i cikla deti skaiciu, kuris parodo kiek pory tikrinsim
     {
-        string wordA=alphaNumericStrings(RLENGTH);
-        string wordB;
-
-        do   //pakeiciame random stringe viena simboli i toki, kad vienas simbolis poroje skirtusi toje pacioje pozicijoje
+        for (int j = 0; j < 25000; j++)
         {
-            //int whichSymbol = rand() % RLENGTH; //randomaizinam kuri simboli keisti nuo 1 iki 5 ( 5 yra similar length kuris gali but keiciamas), arba galim naudot tiesiog skaiciu, bet tiketina kad gali su daugiau poru atsirasti collisiniosas
-            string changedSymbol = alphaNumericStrings(1);
-            wordB = wordA;
-            wordB[3/*whichSymbol*/] = changedSymbol[0];
-            //cout <<wordA<<" "<<wordB<<endl;
-        }
-        while (wordA == wordB);
+            string wordA=alphaNumericStrings(lengths[i]);
+            string wordB;
+            do   //pakeiciame random stringe viena simboli i toki, kad vienas simbolis poroje skirtusi toje pacioje pozicijoje
+            {
+                //int whichSymbol = rand() % RLENGTH; //randomaizinam kuri simboli keisti nuo 1 iki 5 ( 5 yra similar length kuris gali but keiciamas), arba galim naudot tiesiog skaiciu, bet tiketina kad gali su daugiau poru atsirasti collisiniosas
+                string changedSymbol = alphaNumericStrings(1);
+                wordB = wordA;
+                wordB[3/*whichSymbol*/] = changedSymbol[0];
+                //cout <<wordA<<" "<<wordB<<endl;
+            }
+            while (wordA == wordB);
 
-        string hasha = hashing(wordA);
-        string hashb = hashing(wordB);
+            string hasha = hashing(wordA);
+            string hashb = hashing(wordB);
 
-        if (hasha == hashb)
-            coll++;
+            if (hasha == hashb)
+                coll++;
 
-        string binary1 = binary(hasha);
-        string binary2 = binary(hashb);
+            string binary1 = binary(hasha);
+            string binary2 = binary(hashb);
 
-        double difference = compareInput(binary1, binary2);
-        totalDif = totalDif + difference;
-        if (difference < minDif)
-        {
-            minDif = difference;
-        }
-        else if (difference > maxDif)
-        {
-            maxDif = difference;
+            double differenceHex = compareInput(hasha, hashb);
+            double difference = compareInput(binary1, binary2);
+            totalDif = totalDif + difference;
+            totalDifHex = totalDifHex + differenceHex;
+
+            if (differenceHex < minDifHex)
+            {
+                minDifHex = differenceHex;
+            }
+            else if (differenceHex > maxDifHex)
+            {
+                maxDifHex = differenceHex;
+            }
+
+            if (difference < minDif)
+            {
+                minDif = difference;
+            }
+            else if (difference > maxDif)
+            {
+                maxDif = difference;
+            }
         }
     }
+
+    cout <<"Testas bit atzvilgiu : "<<endl;
+
     cout << "Collision'ai: " << coll <<endl;
     cout << "Min diff: " << minDif * 100 << endl;
     cout << "Max diff: " << maxDif * 100 << endl;
     cout << "Average diff: " << (totalDif / RCOUNT) * 100 << endl;
+
+    cout <<"Testas hex atzvilgiu : "<<endl;
+
+    cout << "Collision'ai: " << coll <<endl;
+    cout << "Min diff: " << minDifHex * 100 << endl;
+    cout << "Max diff: " << maxDifHex * 100 << endl;
+    cout << "Average diff: " << (totalDifHex / RCOUNT) * 100 << endl;
 }
